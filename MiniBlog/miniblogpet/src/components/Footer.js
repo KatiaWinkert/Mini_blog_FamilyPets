db90cf25a8d51eec9698a259570a2c231a1cd731
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-19 22:09:48 -0300

    feat
    pagina footer

---
+++ MiniBlog/miniblogpet/src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Blog Famili Pet &copy; 2022</p>
+    </footer>
+  )
+}
+
+export default footer
