8adbf5550544eb8812bff7693f85c39202d96b1a
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-19 22:09:05 -0300

    feat
    inclusão de imports e estrutura react router

---
+++ MiniBlog/miniblogpet/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+import About from './pages/About/About'
+//pages
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
