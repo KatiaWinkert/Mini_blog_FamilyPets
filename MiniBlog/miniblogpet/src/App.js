786e37f3c819e0630a70913df708b5c04a66c7af
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-23 00:11:24 -0300

    perf
    implementação de context

--- MiniBlog/miniblogpet/src/App.js
+++ MiniBlog/miniblogpet/src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import About from './pages/About/About'
+
+//context 
+import { AuthProvider } from './context/AuthContext'
+
+
 //pages
 import Home from './pages/Home/Home'
 import Navbar from './components/Navbar'
@@ -12,18 +17,20 @@ import Login from './pages/Login/Login'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
