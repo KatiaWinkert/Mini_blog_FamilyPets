b02f868a24f7a352dbb03835a290f3d820621bc6
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-25 22:15:34 -0300

    perf
    implementando logout ou sign out

--- MiniBlog/miniblogpet/src/hooks/useAuthentication.js
+++ MiniBlog/miniblogpet/src/hooks/useAuthentication.js
@@ -25,6 +25,8 @@ export const useAuthentication = () => {
       return
     }
   }
+
+  //Register :)
   const createUser = async (data) => {
     checkiFisCancelled()
 
@@ -62,6 +64,12 @@ export const useAuthentication = () => {
     }
   }
 
+  //logout - sign out
+  const logout = () => {
+    checkiFisCancelled()
+    signOut(auth)
+  }
+
   useEffect(() => {
     return () => setCancelled(true)
   }, [])
@@ -71,5 +79,6 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
+    logout,
   }
 }
