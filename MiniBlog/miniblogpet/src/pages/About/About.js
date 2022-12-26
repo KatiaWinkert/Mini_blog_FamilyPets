0cde9db160b278fdf126989eb4317bed8043f0db
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-25 22:04:49 -0300

    perf
    atualizando a pag about

--- MiniBlog/miniblogpet/src/pages/About/About.js
+++ MiniBlog/miniblogpet/src/pages/About/About.js
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
 import styles from './About.module.css'
 
 const About = () => {
   return (
-    <div><h1>About</h1></div>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Blog <span>Familia Pet</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito em React no front-end e Firebase
+        no back-end.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar post
+      </Link>
+    </div>
   )
 }
 
