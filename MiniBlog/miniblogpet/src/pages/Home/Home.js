3d7a76b122f7a8ca56fe4b6b381ee9f199c1b9ee
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-04 14:50:15 -0300

    perf
    Atualizando performance de codigo
    04/01/2023 - 14:50

--- MiniBlog/miniblogpet/src/pages/Home/Home.js
+++ MiniBlog/miniblogpet/src/pages/Home/Home.js
@@ -12,9 +12,14 @@ import PostDetails from '../../components/PostDetails'
 const Home = () => {
   const [query, setQuery] = useState('')
   const { documents: posts, loading } = useFetchDocuments('posts')
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    if (query) {
+      return navigate(`/search?q=${query}`)
+    }
   }
 
   return (
