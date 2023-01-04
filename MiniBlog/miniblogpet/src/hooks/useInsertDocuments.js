83514e65a90f4e0e8c1a79c877cf7df0f35a11b1
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-04 13:26:27 -0300

    perf
    atualizando o hook iseInsertDocuments .js 04/01/2023 - 13:26 hr

--- MiniBlog/miniblogpet/src/hooks/useInsertDocuments.js
+++ MiniBlog/miniblogpet/src/hooks/useInsertDocuments.js
@@ -1,85 +1,60 @@
-//hooks
 import { useState, useEffect, useReducer } from 'react'
-
-//fireBase
 import { db } from '../firebase/config'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
 
-//firebase store
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
-
-//inicia sem loading e sem erro
 const initialState = {
   loading: null,
   error: null,
 }
 
-//Reduce SWITCH -----------------------------------------------
 const insertReducer = (state, action) => {
-  //cada estado de inserção do documento:
   switch (action.type) {
     case 'LOADING':
-      return { loading: true, error: null } // entra em estado de loading
+      return { loading: true, error: null }
     case 'INSERTED_DOC':
-      return { loading: false, error: null } // sai de estado de loading
+      return { loading: false, error: null }
     case 'ERROR':
       return { loading: false, error: action.payload }
     default:
       return state
   }
 }
-// INSERT DOC  recebe uma coleção --------------------
+
 export const useInsertDocument = (docCollection) => {
-  //reducer
   const [response, dispatch] = useReducer(insertReducer, initialState)
 
-  //deal with memory leak (para não ter vazamento de memoria)
-
+  // deal with memory leak
   const [cancelled, setCancelled] = useState(false)
-  // antes de fazer alguma ação valida se esta cancelada.
 
   const checkCancelBeforeDispatch = (action) => {
     if (!cancelled) {
-      //se precisar continuar com hook ou nao.
       dispatch(action)
     }
   }
 
-  //INSERT DOC recebe um document ------------------------------------------------
-  //Inserir o documento.
   const insertDocument = async (document) => {
-    // Carregando o insert
-    checkCancelBeforeDispatch({
-      type: 'LOADING',
-    })
+    checkCancelBeforeDispatch({ type: 'LOADING' })
 
     try {
-      //Cria o objeto e adiciona o campo de timestamp.
-      const newDocument = { ...document, createdAt: serverTimestamp() }
+      const newDocument = { ...document, createdAt: Timestamp.now() }
 
-      //Procura na coleção o documento que recebeu como argumento função
-      const insertDocument = await addDoc(
+      const insertedDocument = await addDoc(
         collection(db, docCollection),
         newDocument
       )
-      //Monta o metodo:
+
       checkCancelBeforeDispatch({
         type: 'INSERTED_DOC',
-        payload: insertDocument,
+        payload: insertedDocument,
       })
     } catch (error) {
-      //Dispara o erro caso aconteça:
-      checkCancelBeforeDispatch({
-        type: 'ERROR',
-        payload: error.message,
-      })
+      checkCancelBeforeDispatch({ type: 'ERROR', payload: error.message })
     }
   }
 
-  //Encerra o componente:
-     useEffect(() => {
-       return () => setCancelled(true)
-     }, [])
+  useEffect(() => {
+    return () => setCancelled(true)
+  }, [])
 
-  //esporta a funão do hook e a resposta.
   return { insertDocument, response }
 }
