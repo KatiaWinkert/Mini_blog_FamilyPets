0b6baacc74b56c43b699002d8b9a36d4f9c6b2f8
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-08 19:55:37 -0300

    firebase
    instalação do novo firebase config

--- MiniBlog/miniblogpet/src/firebase/config.js
+++ MiniBlog/miniblogpet/src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAFwG4miQCqaK-7KDvnJ5U3PhutEvHXIug',
-  authDomain: 'blog-familia-pet.firebaseapp.com',
-  projectId: 'blog-family-pet',
-  storageBucket: 'blog-familia-pet.appspot.com',
-  messageSenderId: '   851141111628 ',
-  appId: '1:851141111628:web:fdb37aec334dadead269e9',
+  apiKey: 'AIzaSyAlJWujDu16ak9TM8AvsKmYJY51296ZIGk',
+  authDomain: 'blogfamiliapet.firebaseapp.com',
+  projectId: 'blogfamiliapet',
+  storageBucket: 'blogfamiliapet.appspot.com',
+  messagingSenderId: '517632204322',
+  appId: '1:517632204322:web:e5ad1cc67772f109ac347b',
 }
 
 const app = initializeApp(firebaseConfig)
