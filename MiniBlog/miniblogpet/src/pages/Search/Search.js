dc1c953db8fc7a8038e54f0e0f363e83ecd4c9d8
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-09 20:51:06 -0300

    Search.js
    Implementando codigos a paginas de busca
    09/01/2023

--- MiniBlog/miniblogpet/src/pages/Search/Search.js
+++ MiniBlog/miniblogpet/src/pages/Search/Search.js
@@ -1,17 +1,33 @@
 import styles from './Search.module.css'
 
-//hook:
+// hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useQuery } from '../../hooks/useQuery'
+
+// components
+import PostDetail from '../../components/PostDetail'
+import { Link } from 'react-router-dom'
 
 const Search = () => {
   const query = useQuery()
   const search = query.get('q')
 
+  const { documents: posts } = useFetchDocuments('posts', search)
+
   return (
-    <div>
-      <h2>Search</h2>
-      <p>{Search}</p>
+    <div className={styles.search_container}>
+      <h1>Resultados encontrados para: {search}</h1>
+      <div className="post-list">
+        {posts && posts.length === 0 && (
+          <>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   )
 }
