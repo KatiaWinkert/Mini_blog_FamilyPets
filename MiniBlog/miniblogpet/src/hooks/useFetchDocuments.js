19510d7c7534a00e9f91a280a22bbad4c9b66f5b
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-09 22:52:44 -0300

    Perf useDocuments.js
    Atualizando hook para melhoria de performance
    09/01/2023 22:53

--- MiniBlog/miniblogpet/src/hooks/useFetchDocuments.js
+++ MiniBlog/miniblogpet/src/hooks/useFetchDocuments.js
@@ -1,71 +1,74 @@
-import { useState, useEffect } from 'react'
-import { db } from '../firebase/config'
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
 import {
   collection,
   query,
   orderBy,
   onSnapshot,
   where,
-  QuerySnapshot,
-} from 'firebase/firestore'
-
+} from "firebase/firestore";
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
-  const [documents, setDocuments] = useState(null)
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(null)
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
   // deal with memory leak
-  const [cancelled, setCancelled] = useState(false)
+  const [cancelled, setCancelled] = useState(false);
 
   useEffect(() => {
     async function loadData() {
-      if (cancelled) return
+      if (cancelled) {
+        return;
+      }
 
-      setLoading(true)
+      setLoading(true);
 
-      const collectionRef = await collection(db, docCollection)
+      const collectionRef = await collection(db, docCollection);
 
       try {
-        let q
-        //busca
-
-        //Dashboard
+        let q;
 
         if (search) {
           q = await query(
             collectionRef,
-            where('tagsArrays', 'array-contains', search),
-            orderBy('createdAt', 'desc')
-          )
+            where("tags", "array-contains", search),
+            orderBy("createdAt", "desc")
+          );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt", "desc")
+          );
         } else {
-          q = await query(collectionRef, orderBy('createdAt', 'desc'))
+          q = await query(collectionRef, orderBy("createdAt", "desc"));
         }
 
-        await onSnapshot(q, (QuerySnapshot) => {
+        await onSnapshot(q, (querySnapshot) => {
           setDocuments(
-            QuerySnapshot.docs.map((doc) => ({
+            querySnapshot.docs.map((doc) => ({
               id: doc.id,
               ...doc.data(),
             }))
-          )
-        })
-
-        setLoading(false)
+          );
+        });
       } catch (error) {
-        console.log('error')
-        setError(error.message)
-
-        setLoading(false)
+        console.log(error);
+        setError(error.message);
       }
+
+      setLoading(false);
     }
 
-    loadData()
-  }, [docCollection, search, uid, cancelled])
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  console.log(documents);
 
   useEffect(() => {
-    return () => setCancelled(true)
-  }, [])
+    return () => setCancelled(true);
+  }, []);
 
-  return { documents, error, loading }
-}
+  return { documents, loading, error };
+};
