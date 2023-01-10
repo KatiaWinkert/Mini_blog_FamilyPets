a93ef0c4ee69fffe9914b703c2406dd8d126b104
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-10 09:30:03 -0300

    perf
    Atualizando os codigos da pagina de Dashboard 10/01/2023 - 9:29

--- MiniBlog/miniblogpet/src/pages/Dashboard/Dashboard.js
+++ MiniBlog/miniblogpet/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,36 @@
 import style from './Dashboard.module.css'
 
+import { Link } from 'react-router-dom'
+
+//hooks
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 const Dashboard = () => {
+  const { user } = useAuthValue()
+  const { uid } = user.uid
+
+  //post do usuario:
+  const { documents: posts, loading } = useFetchDocuments('posts', null, uid)
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={style.noposts}>
+          <p>Não foram encontrados posts!</p>
+          <Link to="/posts/create" className="btn">
+            Criar posts.
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
+
+      {posts && posts.map((post) => (<h3>{post.title}</h3>))}
     </div>
   )
 }
