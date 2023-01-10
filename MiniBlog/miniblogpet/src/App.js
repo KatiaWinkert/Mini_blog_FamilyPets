97f96dfbb57029d536a129894d36cc6da9629529
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-09 22:51:04 -0300

    Atualizando App.js
    incluindo import post e route path
    09/01/2023 22:50

--- MiniBlog/miniblogpet/src/App.js
+++ MiniBlog/miniblogpet/src/App.js
@@ -17,10 +17,13 @@ import Register from './pages/Register/Register'
 import CreatePost from './pages/CreatePost/CreatePost'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Search from './pages/Search/Search'
+import Post from './pages/Post/Post'
+
 
 //components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+
 
 function App() {
   const [user, setUser] = useState(undefined)
@@ -48,6 +51,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
