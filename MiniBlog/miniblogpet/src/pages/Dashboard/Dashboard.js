e09fcd7108b1cd84ae2f78a5c54064065e7eb7cc
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-17 14:43:41 -0300

    Dashborad
    Atualização da interface (corrigindo bug)
    
    17/01/2023 14:43

--- MiniBlog/miniblogpet/src/pages/Dashboard/Dashboard.js
+++ MiniBlog/miniblogpet/src/pages/Dashboard/Dashboard.js
@@ -8,20 +8,20 @@ import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 const Dashboard = () => {
   const { user } = useAuthValue()
-  const { uid } = user.uid
+  const uid = user.uid
 
   //post do usuario:
-  const { documents: posts } = useFetchDocuments('posts', null, uid)
+  const { documents: posts, loading } = useFetchDocuments('posts', null, uid)
 
   return (
     <div>
-      <h2>Dashboard</h2>
-      <p>Gerencie seus posts</p>
+      <h2> Dashboard</h2>
+      <p>Gerencie os seus posts.</p>
       {posts && posts.length === 0 ? (
         <div className={style.noposts}>
-          <p>Não foram encontrados posts!</p>
+          <p>Não foram encontrados posts.</p>
           <Link to="/posts/create" className="btn">
-            Criar o seu primeiro post.
+            Criar primeiro post
           </Link>
         </div>
       ) : (
@@ -29,9 +29,9 @@ const Dashboard = () => {
           <p>Tem posts!</p>
         </div>
       )}
-      {posts && posts.map((post) => (<h3 key={post.id}>{post.title}</h3>))}
+
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   )
 }
-
 export default Dashboard
