e8ec4d8e8b7bd4d30f1ff81ff72500fc652e55d9
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-01-17 22:56:52 -0300

    Dashboard
    Atualização de import -
    17/01/2023 - 22:56

--- MiniBlog/miniblogpet/src/pages/Dashboard/Dashboard.js
+++ MiniBlog/miniblogpet/src/pages/Dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 //hooks
 import { useAuthValue } from '../../context/AuthContext'
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { useDeleteDocument } from '../../hooks/useDeleteDocument'
 
 const Dashboard = () => {
   const { user } = useAuthValue()
@@ -12,7 +13,8 @@ const Dashboard = () => {
 
   //post do usuario:
   const { documents: posts, loading } = useFetchDocuments('posts', null, uid)
-  const deleteDocument = (id) => {}
+
+  const { deleteDocument } = useDeleteDocument('posts')
 
   if (loading) {
     return <p>Carregando...</p>
