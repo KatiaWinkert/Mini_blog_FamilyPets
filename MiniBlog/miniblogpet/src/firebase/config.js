8c13a88960c15628881e83d096056e36528f4d43
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2023-09-13 23:02:37 -0300

    :bomb: Atualização
    Atualização

--- MiniBlog/miniblogpet/src/firebase/config.js
+++ MiniBlog/miniblogpet/src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAlJWujDu16ak9TM8AvsKmYJY51296ZIGk',
-  authDomain: 'blogfamiliapet.firebaseapp.com',
-  projectId: 'blogfamiliapet',
-  storageBucket: 'blogfamiliapet.appspot.com',
-  messagingSenderId: '517632204322',
-  appId: '1:517632204322:web:e5ad1cc67772f109ac347b',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 }
 
 const app = initializeApp(firebaseConfig)
